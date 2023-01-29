1124fce5d624dfa7c46a4cbb4439d11aa7005457
Furkan Kerem Özdemir <119844176+fkozdev@users.noreply.github.com>
2023-01-30 02:10:17 +0300

    Add files via upload

---
+++ exif.py
@@ -0,0 +1,27 @@
+from PIL import Image
+from PIL.ExifTags import TAGS
+import sys
+res = sys.argv[1]
+
+if res == "-r":
+        img = sys.argv[2]
+        image = Image.open(img)
+        info_dict = {
+                "Filename": image.filename,
+                "Image Size": image.size,
+                "Image Height": image.height,
+                "Image Width": image.width,
+                "Image Format": image.format,
+                "Image Mode": image.mode,
+                "Image is Animated": getattr(image, "is_animated", False),
+                "Frames in Image": getattr(image, "n_frames", 1)
+        }
+        for label,value in info_dict.items():
+                print(f"{label:25}: {value}")
+        exifdata = image.getexif()
+        for tag_id in exifdata:
+                tag = TAGS.get(tag_id, tag_id)
+                data = exifdata.get(tag_id)
+                if isinstance(data, bytes):
+                        data = data.decode()
+                print(f"{tag:25}: {data}")
